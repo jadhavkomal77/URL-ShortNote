b88c22e8ce6b639fea3af2d29c1720ee21b83845
Komal Jadhav <komalmjadhav25@gmail.com>
2024-06-14 16:03:19 +0530

    index added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 const cookieParser = require("cookie-parser")
 const { userProtected, adminProtected } = require("./middlewares/protected")
 require("dotenv").config({ path: "./.env" })
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 const app = express()
 //midddlewares
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
@@ -26,8 +28,10 @@ app.use("/api/v1/admin", adminProtected, require("./routes/adminRoutes"))
 
 //404
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "resource Not foudn" })
 })
+
 // error handleer
 app.use((err, req, res, next) => {
     console.log(err)
